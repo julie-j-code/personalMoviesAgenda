b010a301f4ab23a76692514961c219543290744a
Julie J <jeannet.julie@gmail.com>
2022-06-08 12:04:14 +0200

    Affichage des Films enregistrés en fonction de la date sélectionnée

--- script.js
+++ script.js
@@ -16,11 +16,14 @@ const calendar = new VanillaCalendar({
   ],
   shortWeekday: ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"],
   onSelect: (data, elem) => {
-      console.log(data);
-      console.log(elem);
-      // la data récupérée étant un objet avec une propriété date
-      selectedDate = new Date(data.date).toISOString().split("T")[0];
-      alert(`Vous avez sélectionné le ${selectedDate}`);
+    console.log(data);
+    console.log(elem);
+    // la data récupérée étant un objet avec une propriété date
+    selectedDate = new Date(data.date).toISOString().split("T")[0];
+    alert(`Vous avez sélectionné le ${selectedDate}`);
+    // pour afficher les films sauvegardés pour la date sélectionnée
+    displayMovie(new Date(data.date).toISOString());
+
   },
 });
 
@@ -45,7 +48,7 @@ function addMovie(e) {
     year: Number(year),
     duration,
     genres,
-    date: new Date(selectedDate),
+    date: selectedDate,
   };
   console.log(newMovie);
   saveMovie(newMovie);
@@ -58,4 +61,37 @@ function saveMovie(movie) {
   let movies = JSON.parse(localStorage.getItem("movies")) || [];
   movies = [...movies, movie];
   localStorage.setItem("movies", JSON.stringify(movies));
+}
+
+// afficher les films enregistrés
+function displayMovie(date) {
+  let movies = JSON.parse(localStorage.getItem("movies")) || [];
+  console.log(movies);
+  let movie = movies.filter((movie) => {
+    // console.log("date", date.split("T")[0]);
+    // console.log("movie-date", movie.date);
+    return movie.date === date.split("T")[0];
+  });
+
+  console.log("resultat", movie);
+
+  if (movie) {
+    let movieList = document.querySelector("#movie-list");
+    movieList.innerHTML = `
+      <div class="movie-item">
+      <h3>${movie.title}</h3>
+      <span>année : ${movie.year}</span>
+      <span>durée : ${movie.duration}</span>
+      <span>genre : ${movie.genres}</span>
+      </div>
+      `;
+  } else {
+    let movieList = document.querySelector("#movie-list");
+    movieList.innerHTML = `
+      <div class="movie-item">
+      <h3>Aucun film prévu ce jour là</h3>
+      Vous pouvez ajouter un film grâce au formulaire ci-dessous.
+      </div>
+      `;
+  }
 }
