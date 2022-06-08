7146d00c8ed88585d5a45b11f933ad3b4763ecff
Julie J <jeannet.julie@gmail.com>
2022-06-08 09:50:53 +0200

    Sauvegarde des films dans localStorage

--- script.js
+++ script.js
@@ -37,6 +37,25 @@ function addMovie(e) {
   const title = formData.get("title");
   const year = formData.get("year");
   const duration = formData.get("duration");
-  const genres = formData.get("genres");
-  console.log(title, year, duration, genres);
+  // const genres = formData.get("genres");
+  const genres = formData.getAll("genres");
+  // console.log(title, year, duration, genres);
+  const newMovie = {
+    title,
+    year: Number(year),
+    duration,
+    genres,
+    date: new Date(selectedDate.date),
+  };
+  console.log(newMovie);
+  saveMovie(newMovie);
+  movieForm.reset();
+}
+
+// pour sauvegarder dans localStorage
+
+function saveMovie(movie) {
+  let movies = JSON.parse(localStorage.getItem("movies")) || [];
+  movies = [...movies, movie];
+  localStorage.setItem("movies", JSON.stringify(movies));
 }
